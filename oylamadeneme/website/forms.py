79ab0359cc73357ff763a73705e5b83105b7202e
sryldrm <116199724+sryldrm@users.noreply.github.com>
2024-05-25 18:51:02 +0300

    Update forms.py

--- oylamadeneme/website/forms.py
+++ oylamadeneme/website/forms.py
@@ -1,11 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
+from wtforms.fields import DateTimeLocalField, IntegerField
+from wtforms.validators import Optional, NumberRange
 
 class OylamaForm(FlaskForm):
     question = StringField('Question', validators=[DataRequired(), Length(max=100)])
     options = TextAreaField('Options', validators=[DataRequired(), Length(max=500)])
     group_id = SelectField('Group', coerce=int, validators=[DataRequired()])
+    days = IntegerField('Days', validators=[NumberRange(min=0)])
+    hours = IntegerField('Hours', validators=[NumberRange(min=0, max=23)])
+    minutes = IntegerField('Minutes', validators=[NumberRange(min=0, max=59)])
     submit = SubmitField('Create Poll')
 
 class GroupForm(FlaskForm):
